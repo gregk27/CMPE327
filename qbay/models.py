e7d152e21225f0cde3085e95bbbfd3295e56797d
gregk27 <git@gregk.ca>
2021-09-29 09:35:37 -0400

    Merge entites from A1 into template code

--- qbay/models.py
+++ qbay/models.py
@@ -1,30 +1,105 @@
 from qbay import app
+import uuid
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_imageattach.entity import Image, image_attachment
+from sqlalchemy.dialects.postgresql import UUID
 
+db = SQLAlchemy(app)
+
+# Declare a base
+Base = declarative_base()
 
 '''
 This file defines data models and related business logics
 '''
 
-
-db = SQLAlchemy(app)
-
-
 class User(db.Model):
-    username = db.Column(
-        db.String(80), nullable=False)
-    email = db.Column(
-        db.String(120), unique=True, nullable=False,
-        primary_key=True)
-    password = db.Column(
-        db.String(120), nullable=False)
+    """User model."""
+    id = db.Column(db.String, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    balance = db.Column(db.Float, nullable=False)
+    __tablename__ = "user"
 
     def __repr__(self):
         return '<User %r>' % self.username
 
 
+# Source:
+#   https://sqlalchemy-imageattach.readthedocs.io/en/1.1.0/guide/declare.html
+# Used for including product image
+class Product(Base):
+    """Product model."""
+    id = db.Column(db.String, primary_key=True)
+    productName = db.Column(db.String, nullable=False)
+    brand = db.Column(db.String)
+    size = db.Column(db.Float)
+    width = db.Column(db.Float)
+    height = db.Column(db.Float)
+    colour = db.Column(db.String)
+    currentPrice = db.Column(db.Float, nullable=False)
+    startingBid = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    shipCost = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String)
+    bidder = db.Column(db.String)
+    image = image_attachment('ProductPicture')
+    __tablename__ = "product"
+
+
+class ProductPicture(Base, Image):
+    """Product picture model."""
+
+    productId = db.Column(db.String, ForeignKey('product.id'),
+                          primary_key=True)
+    product = relationship('Product')
+    __tablename__ = 'product_picture'
+
+
+# Using postgresql for UUID
+class Sessions(db.Model):
+    """Session model."""
+    userId = db.Column(db.String)
+    sessionId = db.Column(UUID(as_uuid=True),
+                          primary_key=True, default=uuid.uuid4)
+    expiry = db.Column(db.String)
+    ipAddress = db.Column(db.String)
+    csrfToken = db.Column(db.String)
+    __tablename__ = "session"
+
+
+# Used to process transactions
+class Transaction(db.Model):
+    """Transaction model."""
+    paymentId = db.Column(db.String, primary_key=True)
+    customerId = db.Column(db.String, nullable=False)
+    netAmount = db.Column(db.Float, nullable=False)
+    merchant = db.Column(db.String, nullable=False)
+    cardId = db.Column(db.BigInteger, nullable=False, unique=True)
+    cvv = db.Column(db.Integer, nullable=False)
+    expiryDate = db.Column(db.Date(), nullable=False)
+    billAddress = db.Column(db.String, nullable=False)
+    __tablename__ = "transaction"
+
+
+class Review(db.Model):
+    """Product Review model."""
+    id = db.Column(db.String, primary_key=True, unique=True)
+    productId = db.Column(db.String, ForeignKey('product.id'), nullable=False)
+    userId = db.Column(db.String, ForeignKey('user.id'), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.String, nullable=False)
+    datetime = db.Column(db.DateTime, nullable=False)
+    __tablename__ = "review"
+
 # create all tables
-db.create_all()
+# db.create_all()
 
 
 def register(name, email, password):
