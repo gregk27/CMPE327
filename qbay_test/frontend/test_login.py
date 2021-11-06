8661bb6e8ab81dcd7b51ebc7620b5fe2ac67794a
Gregory Kelly <git@gregk.ca>
2021-11-06 19:56:19 -0400

    Overhaul login test to use input/output partitioning based on PR review; #49

--- qbay_test/frontend/test_login.py
+++ qbay_test/frontend/test_login.py
@@ -43,11 +43,10 @@ class FrontEndHomePageTest(BaseCase):
         self.assert_text("Welcome Test User!", "#welcome-header")
         # other available APIs
 
-    def test_login_failure(self, *_):
+    def test_login(self, *_):
         """
         Test that the login fails on bad credentials
-        This is an output test as the input is merely one which
-        achieves the error message
+        It contains both input and output testing methods
         """
         # Ensure the user is in the database, ignore errors from preexistance
         try:
@@ -55,6 +54,15 @@ class FrontEndHomePageTest(BaseCase):
         except ValueError:
             pass
 
+        """
+        Input partitioning:
+         - Bad email, bad password
+         - Good email, bad password
+         - Bad email, good password
+         - Good email, good password
+        """
+
+        # Bad email, bad password partition
         # open login page
         self.open(base_url + '/user/login')
         # fill email and password
@@ -63,12 +71,72 @@ class FrontEndHomePageTest(BaseCase):
         # click enter button
         self.click('input[type="submit"]')
 
-        # after clicking on the browser (the line above)
-        # the front-end code is activated
-        # and tries to call get_user function.
-        # The get_user function is supposed to read data from database
-        # and return the value.
+        # test that it's still on the login page
+        self.assert_text("Log In", "h1")
 
-        # Check for output error message
+        # Bad email, bad password partition
+        # open login page
+        self.open(base_url + '/user/login')
+        # fill email and password
+        self.type("#email", "user@test.com")
+        self.type("#password", "BadPassword1!")
+        # click enter button
+        self.click('input[type="submit"]')
+
+        # test that it's still on the login page
+        self.assert_text("Log In", "h1")
+
+        # Bad email, bad password partition
+        # open login page
+        self.open(base_url + '/user/login')
+        # fill email and password
+        self.type("#email", "user2@test.com")
+        self.type("#password", "ValidPass1!")
+        # click enter button
+        self.click('input[type="submit"]')
+
+        # test that it's still on the login page
+        self.assert_text("Log In", "h1")
+
+        # Bad email, bad password partition
+        # open login page
+        self.open(base_url + '/user/login')
+        # fill email and password
+        self.type("#email", "user@test.com")
+        self.type("#password", "ValidPass1!")
+        # click enter button
+        self.click('input[type="submit"]')
+
+        # test if the page loads correctly
+        self.assert_element("#welcome-header")
+        self.assert_text("Welcome Test User!", "#welcome-header")
+
+        """
+        Output partitioning
+         - displays error
+         - redirects to homepage
+        """
+        # displays error
+        # open login page
+        self.open(base_url + '/user/login')
+        # fill email and password
+        self.type("#email", "user2@test.com")
+        self.type("#password", "BadPassword1!")
+        # click enter button
+        self.click('input[type="submit"]')
+
+        # test that the login error is displayed
         self.assert_text("Incorrect email or password", "#message")
-        # other available APIs
+
+        # redirects to homepage
+        # open login page
+        self.open(base_url + '/user/login')
+        # fill email and password
+        self.type("#email", "user@test.com")
+        self.type("#password", "ValidPass1!")
+        # click enter button
+        self.click('input[type="submit"]')
+
+        # test if it's on the homepage
+        self.assert_element("#welcome-header")
+        self.assert_text("Welcome Test User!", "#welcome-header")
